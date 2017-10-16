28de8e8f33e58e02f6da624a939cae1a3f5bf09e
liangfei <njliangfei@gmail.com>
2017-10-16 20:58:29 +0800

    🌹add options

--- seeker.js
+++ seeker.js
@@ -1,31 +1,78 @@
-const rp = require('request-promise')
-const cheerio = require('cheerio')
-const parseArgs = require('minimist')
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const commandLineArgs = require('command-line-args');
+const getUsage = require('command-line-usage');
 
-let args = parseArgs(process.argv.slice(2));
-let destUrl = args._[0];
+const optionDefinitions = [
+    { 
+        name: 'url', 
+        alias: 'u',
+        type: String, 
+        typeLabel: '[underline]{default option}',
+        defaultOption: true 
+    },
+    { 
+        name: 'protocol', 
+        alias: 'p', 
+        type: String, 
+        defaultValue: 'ed2k',
+        typeLabel: '[[underline]{e2dk}|[underline]{magnet}]'
+    },
+    { 
+        name: 'tab', 
+        alias: 't', 
+        type: String, 
+        defaultValue: 'tab-g101-HR-HDTV',
+        typeLabel: '[underline]{tab-g101-HR-HDTV}'
+    },
+    { 
+        name: 'help', 
+        alias: 'h', 
+        type: Boolean 
+    },
+]
 
-if (!destUrl) {
-    console.log('please sepecifiy destination url');
+const args = commandLineArgs(optionDefinitions);
+
+const usage = [
+    {
+        header: 'DoramaSeeeeeeker',
+        content: 'Fetch dramas for you'
+    },
+    {
+        header: 'Options',
+        optionList: optionDefinitions
+    },
+    {
+        header: 'Synopsis',
+        content: [
+          '$ seeker [underline]{http://xiazai002.com/myMOm2} [bold]{--protocol} [underline]{e2dk} [bold]{--tab} [underline]{tab-g101-HR-HDTV} ...',
+          '$ seeker [bold]{--help}'
+        ]
+    }
+];
+
+if (args.help) {
+    console.log(getUsage(usage))
     return;
 }
 
 var options = {
-    uri: destUrl,
+    uri: args.url,
     transform: (body) => {
         return cheerio.load(body);
     }
 };
 
 rp(options)
-    .then( ($) => {
-        $('div#tab-g101-HR-HDTV ul.down-list').find('a.btn').each((i, val) => {
+    .then(($) => {
+        $(`div#${args.tab} ul.down-list`).find('a.btn').each((i, val) => {
             let link = $(val).attr('href').toString();
-            if (link.startsWith('magnet:')) {
-                console.log(link, '\n');
+            if (link.startsWith(args.protocol)) {
+                console.log(link);
             }
         });
     })
     .catch(err => {
-        //
+        consle.log(`exception with ${err}`);
     });
